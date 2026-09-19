0fffcf663ac7db886df7ce4b1aba2112dd12f715
Anton Vasilev <anton.vasilev@ispring.institute>
2026-09-19 20:06:47 +0300

    chiper fix

--- CaesarCipher/src/Main.java
+++ CaesarCipher/src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.Math;
 public class Main {
     private static char[] fromStringToCharArray(String text) {
         int length = text.length();
@@ -28,13 +27,13 @@ public class Main {
         for (int i = 0; i < symbols.length; i++) {
             if (Character.isLetter(symbols[i])) {
                 if ('A' <= symbols[i] && symbols[i] <= 'Z') {
-                    symbols[i] = (char) ((symbols[i] - 'A' + shift) % 26 + 'A');
+                    symbols[i] = (char) ((symbols[i] - 'A' + (shift % 26)) % 26 + 'A');
                 } else if ('a' <= symbols[i] && symbols[i] <= 'z') {
-                    symbols[i] = (char) ((symbols[i] - 'a' + shift) % 26 + 'a');
+                    symbols[i] = (char) ((symbols[i] - 'a' + (shift % 26)) % 26 + 'a');
                 } else if ('А' <= symbols[i] && symbols[i] <= 'Я') {
-                    symbols[i] = (char) ((symbols[i] - 'А' + shift) % 33 + 'А');
+                    symbols[i] = (char) ((symbols[i] - 'А' + (shift % 32)) % 32 + 'А');
                 } else if ('а' <= symbols[i] && symbols[i] <= 'я') {
-                    symbols[i] = (char) ((symbols[i] - 'а' + shift) % 33 + 'а');
+                    symbols[i] = (char) ((symbols[i] - 'а' + (shift % 32)) % 32 + 'а');
                 }
             }
         }
@@ -52,17 +51,15 @@ public class Main {
                 } else if ('a' <= symbols[i] && symbols[i] <= 'z') {
                     symbols[i] = (char) ((symbols[i] - 'a' - (shift % 26) + 26) % 26 + 'a');
                 } else if ('А' <= symbols[i] && symbols[i] <= 'Я') {
-                    symbols[i] = (char) ((symbols[i] - 'А' - (shift % 26) + 26) % 33 + 'А');
+                    symbols[i] = (char) ((symbols[i] - 'А' - (shift % 32) + 32) % 32 + 'А');
                 } else if ('а' <= symbols[i] && symbols[i] <= 'я') {
-                    symbols[i] = (char) ((symbols[i] - 'а' - (shift % 26) + 26) % 33 + 'а');
+                    symbols[i] = (char) ((symbols[i] - 'а' - (shift % 32) + 32) % 32 + 'а');
                 }
             }
         }
 
         return fromCharArrayToString(symbols);
     }
-
-
 
     public static void main(String[] args) {
         String mode = args[0];
