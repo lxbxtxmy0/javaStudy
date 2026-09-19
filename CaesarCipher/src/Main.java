76fc57045bd0c08aec21b9c0e79cdaf0ff695a06
Anton Vasilev <anton.vasilev@ispring.institute>
2026-09-19 20:09:44 +0300

    chiper final fix

--- CaesarCipher/src/Main.java
+++ CaesarCipher/src/Main.java
@@ -27,13 +27,13 @@ public class Main {
         for (int i = 0; i < symbols.length; i++) {
             if (Character.isLetter(symbols[i])) {
                 if ('A' <= symbols[i] && symbols[i] <= 'Z') {
-                    symbols[i] = (char) ((symbols[i] - 'A' + (shift % 26)) % 26 + 'A');
+                    symbols[i] = (char) ((symbols[i] - 'A' + (shift % 26) + 26) % 26 + 'A');
                 } else if ('a' <= symbols[i] && symbols[i] <= 'z') {
-                    symbols[i] = (char) ((symbols[i] - 'a' + (shift % 26)) % 26 + 'a');
+                    symbols[i] = (char) ((symbols[i] - 'a' + (shift % 26) + 26) % 26 + 'a');
                 } else if ('А' <= symbols[i] && symbols[i] <= 'Я') {
-                    symbols[i] = (char) ((symbols[i] - 'А' + (shift % 32)) % 32 + 'А');
+                    symbols[i] = (char) ((symbols[i] - 'А' + (shift % 32) + 32) % 32 + 'А');
                 } else if ('а' <= symbols[i] && symbols[i] <= 'я') {
-                    symbols[i] = (char) ((symbols[i] - 'а' + (shift % 32)) % 32 + 'а');
+                    symbols[i] = (char) ((symbols[i] - 'а' + (shift % 32) + 32) % 32 + 'а');
                 }
             }
         }
